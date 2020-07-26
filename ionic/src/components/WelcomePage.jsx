c992ecb232fb292d5d839fe56323c7748f53848a
sachix1001 <sachix1001@gmail.com>
2020-07-26 18:08:04 +1000

    add button on mood quiz

--- ionic/src/components/WelcomePage.jsx
+++ ionic/src/components/WelcomePage.jsx
@@ -1,22 +1,11 @@
-import React, { Link } from "react";
-import { Redirect, Route } from "react-router-dom";
+import React from "react";
 import oscar from "../assets/oscar.png";
 import logo from "../assets/logo.png";
-import { IonReactRouter } from "@ionic/react-router";
 import {
   IonButton,
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
+
 } from "@ionic/react";
 import "./WelcomePage.css";
-import MoodQuiz from "./MoodQuiz";
-import Home from "../pages/Home";
-import Tab3 from "../pages/Tab3";
 
 const WelcomePage = () => {
   return (
