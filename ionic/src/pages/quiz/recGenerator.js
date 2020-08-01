dcb135e6968b475876a974d702d5fe579100d05e
sachix1001 <sachix1001@gmail.com>
2020-08-01 18:34:54 +1000

    add default backpage

---
+++ ionic/src/pages/quiz/recGenerator.js
@@ -0,0 +1,23 @@
+function generateRec(mood){
+    const {physicalMood, mentalMood} = mood;
+    if(mentalMood === 'good' && physicalMood === 'good'){
+    }
+    if(mentalMood === 'good' && physicalMood === 'ok'){
+    }
+    if(mentalMood === 'good' && physicalMood === 'bad'){
+    }
+    if(mentalMood === 'ok' && physicalMood === 'good'){
+    }
+    if(mentalMood === 'ok' && physicalMood === 'ok'){
+    }
+    if(mentalMood === 'ok' && physicalMood === 'bad'){
+    }
+    if(mentalMood === 'bad' && physicalMood === 'good'){
+    }
+    if(mentalMood === 'bad' && physicalMood === 'ok'){
+    }
+    if(mentalMood === 'bad' && physicalMood === 'bad'){
+    }
+  }
+  
+  export default generateRec
