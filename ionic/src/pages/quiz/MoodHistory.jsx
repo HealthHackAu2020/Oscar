c601101386ed8c2f9215926fff6308105c0eb894
eug48 <eug48.github@gmail.com>
2020-08-01 19:12:27 +1000

    added mood history page

---
+++ ionic/src/pages/quiz/MoodHistory.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import {
+  IonHeader,
+  IonPage,
+  IonList,
+  IonItem,
+  IonRange,
+  IonLabel,
+  IonButton,
+  IonBackButton,
+  IonContent,
+} from "@ionic/react";
+import { arrowForwardSharp } from "ionicons/icons";
+import Calendar from 'react-calendar'
+import {  useDispatch } from 'react-redux'
+
+
+export function MoodHistoryPage(props) {
+  const { history } = props;
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonBackButton defaultHref="/quiz/MoodQuiz"/>
+      </IonHeader>
+
+      <IonContent>
+        <h1>Mood history</h1>
+
+        <div>
+          Overview of moods this month.
+          Click on one of the days to view more information.
+
+        </div>
+        <IonButton
+          className="next-btn"
+          color="light"
+          shape="round"
+          onClick={(e) => {
+            history.push("/quiz/MentalQuiz");
+          }}
+        >
+          Complete mood quiz
+        </IonButton>
+
+
+        <Calendar />
+
+
+      </IonContent>
+    </IonPage>
+  )
+
+}
