2bcaa4fb49eb1db6745bf136025d6bf26fab78b0
sachix1001 <sachix1001@gmail.com>
2020-08-01 12:29:11 +1000

    update button color

--- ionic/src/theme/theme.js
+++ ionic/src/theme/theme.js
@@ -1,10 +1,10 @@
 export default {
   primaryBlue: "#b4d7eb",
   primaryPurple: "#dfd0ec",
-  primaryGreen: "#afdedb",
-  primaryPink: "#f2b6be",
+  primaryGreen: "#79C8C5",
+  primaryPink: "#F2C4D4",
   secondaryBlue: "#75c7dd",
-  primaryYellow: "#fde7b3",
+  primaryYellow: "#FFD780",
   primaryBlack: "#000000",
   primaryWhite: "#ffffff",
   primaryGray: "#f7f7f7",
