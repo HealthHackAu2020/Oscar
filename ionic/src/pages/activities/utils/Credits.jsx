0a74751580290448d217f0d00d921a1f7c8a6589
eug48 <eug48.github@gmail.com>
2020-08-02 10:07:21 +1000

    activity credits: fixed colours

--- ionic/src/pages/activities/utils/Credits.jsx
+++ ionic/src/pages/activities/utils/Credits.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 
 export function TameYourGut() {
     return (
-      <p> Psychological Therapy Program for People Living with Inflammatory Bowel Disease.
-        <a href="https://www.tameyourgut.com">www.tameyourgut.com</a> Mikocka-Walus, A
+      <p className="activity-credit"> Psychological Therapy Program for People Living with Inflammatory Bowel Disease. <a href="https://www.tameyourgut.com">www.tameyourgut.com</a> Mikocka-Walus, A
       </p>
     )
 }
 
 export function AnxietyCanada() {
     return (
-      <p> 
+      <p className="activity-credit"> 
         <a href="https://www.anxietycanada.com/free-downloadable-pdf-resources/">Anxiety Canada</a>
       </p>
     )
